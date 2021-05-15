5e43a954c2d694906c5d70b14d3a323d6c5944fe
kenny.cai <kennycai2836@163.com>
2021-05-15 12:08:43 +0800

    DiscoveryClient

--- cloud-provider-payment8001/src/main/java/com/learning/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/learning/springcloud/controller/PaymentController.java
@@ -4,15 +4,20 @@ import com.learning.springcloud.entities.CommonResult;
 import com.learning.springcloud.entities.Payment;
 import com.learning.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
 public class PaymentController {
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
@@ -22,5 +27,14 @@ public class PaymentController {
     @GetMapping(value = "/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable Integer id) {
         return new CommonResult<>(200, "success", paymentService.getPaymentById(Long.valueOf(id)));
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery () {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("service:" + service);
+        }
+        return discoveryClient;
     }
 }
